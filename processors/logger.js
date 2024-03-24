335a5bea06b04638783eecbcb3011b6a5da705eb
raphaelpaiva <raphaeldpaiva@gmail.com>
2024-03-24 11:52:39 -0300

    refactor: Some well-deserved class system.

--- processors/logger.js
+++ processors/logger.js
@@ -1,60 +1,48 @@
-const amqp = require('amqplib');
 const fs   = require('node:fs');
 const path = require('path');
+const Processor = require('./processor').Processor;
 
 const logPath = path.resolve(__dirname, 'log');
-const queueName = 'log';
-let globalConnection = undefined;
 
-async function main() {
-  try {
+class Logger extends Processor {
+  constructor() {
+    super('log');
+    this.logPath = path.resolve(__dirname, 'log');
     if (!fs.existsSync(logPath)) {
-      console.log(`[Logger] Creating path ${logPath}`)
+      this.log(`Creating path ${logPath}`)
       fs.mkdirSync(logPath);
     }
-    console.log("[Logger] Simbora");
-    const connection = await amqp.connect('amqp://queue:5672');
-    globalConnection = connection;
-    const channel = await connection.createChannel();
-    await channel.assertQueue(queueName, {durable: true});
-  
-    console.log('[Logger] Registering consumer...');
-    channel.consume(queueName, processMessage(channel));
-  } catch (error) {
-    console.error(`[Logger] ${error}`);
-    terminate();
   }
-}
 
-function processMessage(channel) {
-  return function logMessage(message) {
+  consumer(message) {
     try {
       const logFile = path.resolve(logPath, 'message.log');
       const zapMsg = JSON.parse(message.content.toString());
       const messageString = JSON.stringify(zapMsg);
       fs.appendFile(logFile, `\n${messageString}`, function (err) {
         if (err) {
-          console.error(`[Logger] Error writing to message log: ${err}`);
+          this.log(`Error writing to message log: ${err}`);
           throw err;
         }
       });
-      channel.ack(message);
+      this.channel.ack(message);
     } catch (error) {
-      console.error(`[Logger] ${error}`);
+      console.log('Eita', this, error);
     }
   }
 }
 
-process.on('SIGTERM', () => {
-  console.info('[Logger] SIGTERM signal received.');
-  terminate();
-});
-
-main();
-
-function terminate() {
-  if (globalConnection) {
-    console.log('[Logger] Closing queue connection');
-    globalConnection.close();
-  }
+const logger = new Logger();
+try {
+  logger.connect().then(
+    () => logger.register()
+  );
+  
+} catch (error) {
+  logger.terminate();
 }
+  
+process.on('SIGTERM', () => {
+  logger.log('SIGTERM signal received.');
+  logger.terminate();
+});
